9377a0928bf9003006b86435794cc4ed1098ca1b
Hallan-Lima <54196012+Hallan-Lima@users.noreply.github.com>
2022-05-22 16:09:14 -0300

    Portfolio

---
+++ index.php
@@ -0,0 +1,115 @@
+<?php
+
+//link </a href=''> de contato  
+$contatoTel       = 'http://api.whatsapp.com/send?1=pt_BR&phone=55019997202594';
+$contatoFac       = 'https://www.facebook.com/3301NNY';
+$contatoLinkedin  = 'https://www.linkedin.com/in/h%C3%A1llan/';
+$contatoSkype     = 'https://join.skype.com/invite/DeIl7tg4Ohxd';
+$contatoGithub    = 'https://github.com/Hallan-Lima';
+$contatoEmail     = 'hallansrl@hotmail.com';
+
+?>
+<!DOCTYPE html>
+<html lang="pt-BR">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Portfolio - Hállan Lima</title>
+
+  <!-- estrutura desing modelo -->
+  <link rel="stylesheet" href="css/nicepage.css" media="screen">
+  <link rel="stylesheet" href="css/modelo-2.css" media="screen">
+  <script class="u-script" type="text/javascript" src="js/jquery.js"></script>
+  <script class="u-script" type="text/javascript" src="js/nicepage.js"></script>
+  <link id="u-theme-google-font" rel="stylesheet"
+    href="https://fonts.googleapis.com/css?family=Roboto:100,100i,300,300i,400,400i,500,500i,700,700i,900,900i|Open+Sans:300,300i,400,400i,500,500i,600,600i,700,700i,800,800i">
+  <link id="u-page-google-font" rel="stylesheet"
+    href="https://fonts.googleapis.com/css?family=Raleway:100,100i,200,200i,300,300i,400,400i,500,500i,600,600i,700,700i,800,800i,900,900i|Roboto:100,100i,300,300i,400,400i,500,500i,700,700i,900,900i|Barlow:100,100i,200,200i,300,300i,400,400i,500,500i,600,600i,700,700i,800,800i,900,900i">
+
+</head>
+
+<body class="u-body u-xl-mode">
+  <section class="u-align-left u-clearfix u-image u-section-1">
+    <div class="u-clearfix u-sheet u-valign-middle u-sheet-1">
+      <div class="u-clearfix u-expanded-width u-layout-wrap u-layout-wrap-1">
+        <div class="u-gutter-0 u-layout">
+          <div class="u-layout-row">
+            <div
+              class="u-align-left u-container-style u-layout-cell u-left-cell u-shape-rectangle u-size-27 u-layout-cell-1">
+              <div class="u-container-layout u-valign-middle u-container-layout-1">
+                <h1 class="u-custom-font u-text u-text-body-alt-color u-text-1">Olá,</h1>
+                <p class="u-text u-text-body-alt-color u-text-2">meu nome é Hállan,
+                  atualmente estou me formando em Eng. Computação e sou apaixonado pelo que faço, busco sempre
+                  transformar ideias e sonhos em realidade,
+                  com isso, a cada dia busco aprender uma coisa nova.</p>
+                <a href="<?php echo $contatoTel; ?>"
+                  class="u-active-palette-4-base u-btn u-btn-round u-button-style u-custom-font u-font-raleway u-hover-palette-4-base u-palette-1-base u-radius-50 u-text-body-alt-color u-btn-2">Entre
+                  em contato</a>
+              </div>
+            </div>
+            <div class="u-align-left u-container-style u-image u-layout-cell u-right-cell u-size-33 u-image-1"
+              data-image-width="1372" data-image-height="1235">
+              <div class="u-container-layout u-valign-bottom u-container-layout-2"></div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+
+  <section class="u-clearfix u-white u-section-8" id="sec-d1be">
+    <div class="u-clearfix u-sheet u-sheet-1">
+      <div class="u-clearfix u-expanded-width u-layout-wrap u-layout-wrap-1">
+        <div class="u-layout">
+          <div class="u-layout-row">
+            <div class="u-container-style u-layout-cell u-left-cell u-size-30 u-layout-cell-1">
+              <div class="u-container-layout u-valign-top u-container-layout-1">
+                <h1 class="u-text u-text-body-color u-text-1">Desenvolvedor WEB - Freelancer</h1>
+                <div class="u-border-6 u-border-palette-1-base u-line u-line-horizontal u-line-1"></div>
+                <img
+                  class="u-expanded-width-md u-expanded-width-sm u-expanded-width-xs u-image u-image-contain u-image-1"
+                  src="img/ffff.jpg" data-image-width="1200" data-image-height="800">
+              </div>
+            </div>
+            <div class="u-container-style u-layout-cell u-right-cell u-size-30 u-layout-cell-2">
+              <div class="u-container-layout u-valign-top u-container-layout-2">
+                <p class="u-text u-text-black u-text-2">
+                  <span class="u-text-grey-50">Realizo freelancer de desenvolvedor web, minha missão é desenvolver sites
+                    com ótima qualidade, eficazes e que tenham uma boa acessibilidade.
+                    Você pode contar comigo em relação aos prazos e trabalhar com desafios de um
+                    desenvolvedor!&nbsp;</span>
+                  <br>
+                  <br>Você pode contar comigo em relação aos prazos e trabalhar com desafios de um desenvolvedor!
+                  Tenho criatividade nos desenvolvimentos e sempre busco novos aprendizados,
+                  tento manter um bom relacionamento com o cliente gerando dinâmica e flexibilidade nos projetos.&nbsp;
+                </p>
+                <a href="<?php echo $contatoTel; ?>"
+                  class="u-active-palette-4-base u-align-left u-border-0 u-btn u-btn-round u-button-style u-custom-font u-font-raleway u-hover-palette-4-base u-palette-1-base u-radius-50 u-text-body-alt-color u-btn-1">
+                  Entrar em contato</a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+  <iframe src="template/session-1.php" frameborder="0" width="100%" height="800px"></iframe>
+  <section class="u-align-center-xs u-clearfix u-section-4" src="" id="carousel_46bf">
+    <div class="u-clearfix u-sheet u-valign-middle-xl u-sheet-1">
+      <img class="u-expanded-width-md u-image u-image-contain u-image-1" src="img/Untitled-1-min.jpg">
+      <div
+        class="u-align-center-md u-align-center-sm u-align-center-xs u-border-no-bottom u-border-no-left u-border-no-right u-border-no-top u-container-style u-expanded-width-md u-expanded-width-sm u-expanded-width-xs u-group u-shape-rectangle u-group-1">
+        <div class="u-container-layout u-valign-middle u-container-layout-1">
+          <h1 class="u-text u-text-palette-1-base u-text-1">Titulo</h1>
+          <p class="u-large-text u-text u-text-variant u-text-2">msg</p>
+        </div>
+      </div>
+    </div>
+  </section>
+
+  <footer></footer>
+</body>
+
+</html>
